a1d970eb43710825d27af8a8b82d181bb85cae8a
Aftaab Mulla <aftaabmulla228@gmail.com>
2026-02-10 10:49:11 +0530

    Initial Commit

---
+++ AutoLayoutsTests/AutoLayoutsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutoLayoutsTests.swift
+//  AutoLayoutsTests
+//
+//  Created by Aftaab Mulla on 10/02/26.
+//
+
+import Testing
+@testable import AutoLayouts
+
+struct AutoLayoutsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
